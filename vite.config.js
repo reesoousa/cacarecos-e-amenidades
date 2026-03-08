260ea9e37d77a30101c916fdc3b8ec77bec300ad
Renan Sousa <reesoousa.steam@gmail.com>
2026-03-07 23:16:45 -0300

    chore: finalmente on-air no githubs page

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ export default defineConfig({
   plugins: [react()],
   // Para GitHub Pages: substitua "nome-do-repositorio" pelo nome real do seu repositório.
   // Se publicar no domínio root (user.github.io), use base: '/'.
-  base: '/nome-do-repositorio/'
+  base: '/cacarecos-e-amenidades/'
 })
