b6454e5fc124c4a8093fec51c9a306b3cd04c82e
Renan Sousa <reesoousa.steam@gmail.com>
2026-06-02 19:24:48 -0300

    CONECTANDO O DOMÍNIO

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ export default defineConfig({
   plugins: [react()],
   // Para GitHub Pages: substitua "nome-do-repositorio" pelo nome real do seu repositório.
   // Se publicar no domínio root (user.github.io), use base: '/'.
-  base: '/cacarecos-e-amenidades/'
+  base: '/'
 })
